f60aa7d0603baa3d7f2aec57a8eed78a74f51941
SquancherSquanch <squanchersquanch@yahoo.com>
2017-02-24 06:37:21 -0600

    ...

---
+++ HICO/assets/js/main.js
@@ -0,0 +1,42 @@
+(function($) {
+
+	skel
+		.breakpoints({
+			desktop: '(min-width: 737px)',
+			tablet: '(min-width: 737px) and (max-width: 1200px)',
+			mobile: '(max-width: 736px)'
+		})
+		.viewport({
+			breakpoints: {
+				tablet: {
+					width: 1080
+				}
+			}
+		});
+
+	$(function() {
+
+		var $window = $(window),
+			$body = $('body');
+
+		// Fix: Placeholder polyfill.
+			$('form').placeholder();
+
+		// Prioritize "important" elements on mobile.
+			skel.on('+mobile -mobile', function() {
+				$.prioritize(
+					'.important\\28 mobile\\29',
+					skel.breakpoint('mobile').active
+				);
+			});
+			// Dropdowns.
+			$('#nav > ul').dropotron({
+				mode: 'fade',
+				noOpenerFade: true,
+				hoverDelay: 150,
+				hideDelay: 350
+			});
+
+	});
+
+})(jQuery);
